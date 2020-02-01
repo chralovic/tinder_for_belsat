51d86bd022135db790b1555f1667b6a99656d1ca
Nasta Chrałovič <49338736+chralovic@users.noreply.github.com>
2020-02-02 00:04:58 +0100

    Add files via upload

--- tinder_js.js
+++ tinder_js.js
@@ -8,17 +8,49 @@ function startGame() {
 document.querySelector(".main-container__button").addEventListener('click', startGame);
 
 
-
+const likedProfiles = [];
 
 for (let i=1; i<3; i++) {
-	function invisibleProfile() {
+	function like() {
+	document.querySelector('#nr' + i ).classList.add('invisible');
+	document.querySelector('#nr' + (1 + i)).classList.remove('invisible');
+	const likedDiv = document.querySelector('#nr' + i);
+	likedProfiles.push(likedDiv);
+	return likedProfiles;	
+	}
+	document.querySelector(".like" + i).addEventListener( 'click', like );
+}
+
+for (let i=1; i<3; i++) {
+	function dislike() {
 	document.querySelector('#nr' + i ).classList.add('invisible');
 	document.querySelector('#nr' + (1 + i)).classList.remove('invisible');
 	}
-
-	document.querySelector(".like" + i).addEventListener( 'click', invisibleProfile );
+	document.querySelector(".dislike" + i).addEventListener( 'click', dislike );
 }
 
 
 
 
+function kotek() {
+	let psotek = likedProfiles;
+	console.log( psotek );
+}
+
+document.querySelector(".dislike3").addEventListener( 'click', kotek );
+
+
+/*
+
+
+function getElementsByClassName( strClassName, obj ) {
+    if ( obj.className == strClassName ) {
+        //insert this elm into array 
+        array.push(obj);
+    }  
+}
+
+let likedProfiles = [];
+likedProfiles.prototype.push();
+
+*/
