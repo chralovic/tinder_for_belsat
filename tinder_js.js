24822727d8c1c4f6516997f8c9bdd9ac06923ef0
Nasta Chrałovič <49338736+chralovic@users.noreply.github.com>
2020-02-02 02:05:16 +0100

    Add files via upload

--- tinder_js.js
+++ tinder_js.js
@@ -8,17 +8,48 @@ function startGame() {
 document.querySelector(".main-container__button").addEventListener('click', startGame);
 
 
+const likedProfiles = [];
 
+for (let i=1; i<4; i++) {
+	function like() {
+	document.querySelector('#nr' + i ).classList.add('invisible');
+	document.querySelector('#nr' + (1 + i)).classList.remove('invisible');
+	const likedDiv = document.querySelector('#nr' + i);
+	likedProfiles.push(likedDiv);
+	return likedProfiles;	
+	}
+	document.querySelector(".like" + i).addEventListener( 'click', like );
+}
 
-for (let i=1; i<3; i++) {
-	function invisibleProfile() {
+for (let i=1; i<4; i++) {
+	function dislike() {
 	document.querySelector('#nr' + i ).classList.add('invisible');
 	document.querySelector('#nr' + (1 + i)).classList.remove('invisible');
 	}
-
-	document.querySelector(".like" + i).addEventListener( 'click', invisibleProfile );
+	document.querySelector(".dislike" + i).addEventListener( 'click', dislike );
 }
 
 
+function cleanProfiles(){
+	document.querySelector('#nr4').classList.add('invisible');
+	document.querySelector('#finalMatch').classList.remove('invisible');
+}
 
+document.querySelector(".dislike4").addEventListener( 'click', cleanProfiles );
+document.querySelector(".like4").addEventListener( 'click', cleanProfiles );
+
+
+function randomProfile() {
+	document.querySelector('#finalMatch').classList.add('invisible');
+	let numberMatches = likedProfiles.length;
+	let i = Math.floor(Math.random() * numberMatches);
+	console.log( likedProfiles[i] );
+	likedProfiles[i];
+}
+
+document.querySelector("#finalMatchButton").addEventListener( 'click', randomProfile );
+
+/*
+--Гэтая хуйня паказвае рандомны профіль з усіх профіляў, а не з табліцы: document.querySelector('#nr'+ i).classList.remove('invisible'); -- 
+*/
 
